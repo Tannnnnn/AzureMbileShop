bfb8b9a08ecfc272cbdc9299bcc93f75fae5553d
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-11-18 15:48:20 +0700

    no message

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>AzureMobileShop</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Hello Azure</h1>
+    </div>
+  </body>
+</html>
